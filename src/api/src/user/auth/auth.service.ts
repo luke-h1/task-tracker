c4ea873cbf98b71f9714f30598cec1d59b13b110
luke-h1 <luke.howsam@yahoo.com>
2022-03-13 11:49:05 +0000

    feat(api): add signup service method

---
+++ src/api/src/user/auth/auth.service.ts
@@ -0,0 +1,56 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserType } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+
+interface SignupParams {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+interface SigninParams {
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup(body: SignupParams, userType: UserType): Promise<string> {
+    const exists = await this.prismaService.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+    if (exists) {
+      throw new ConflictException();
+    }
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        ...body,
+        user_type: userType,
+        password: hashedPassword,
+      },
+    });
+    return this.generateJWT(user.firstName, user.id);
+  }
+
+  private generateJWT(name: string, id: number): string {
+    return jwt.sign(
+      {
+        name,
+        id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 3600,
+      },
+    );
+  }
+}
