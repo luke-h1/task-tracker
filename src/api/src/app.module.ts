3c13fe765e6f583f16ba6dce61417f9aa93055fc
luke-h1 <luke.howsam@yahoo.com>
2022-03-13 11:41:28 +0000

    feat(api): setup prisma service

--- src/api/src/app.module.ts
+++ src/api/src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { TaskController } from './task/task.controller';
 import { TaskService } from './task/task.service';
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, TaskModule],
+  imports: [UserModule, TaskModule, PrismaModule],
   controllers: [AppController, TaskController],
   providers: [AppService, TaskService],
 })
