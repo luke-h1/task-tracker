4ed2403f204dc7ed09bece48c2754ed9b0814bde
luke-h1 <luke.howsam@yahoo.com>
2022-03-13 12:04:34 +0000

    feat(api): add signup controller method

--- src/api/src/user/auth/auth.controller.ts
+++ src/api/src/user/auth/auth.controller.ts
@@ -1,10 +1,37 @@
 import * as bcrypt from 'bcryptjs';
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseEnumPipe,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from '../dtos/SignupDto';
+import { UserType } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // @Post('/signup/:userType')
+  @Post('/signup/:userType')
+  async signup(
+    @Body() body: SignupDto,
+    @Param('userType', new ParseEnumPipe(UserType)) userType: UserType,
+  ) {
+    // user is becoming an admin & has an accessKey from an admin user
+    if (userType !== UserType.DEFAULT && body.accessKey) {
+      const validAccessKey = `${body.email}-${userType}-${process.env.ACCESS_KEY_SECRET}`;
+
+      const isValidAccessKey = await bcrypt.compare(
+        validAccessKey,
+        body.accessKey,
+      );
+      if (!isValidAccessKey) {
+        throw new UnauthorizedException();
+      }
+    }
+    return this.authService.signup(body, userType);
+  }
 }
---
+++ src/api/src/user/dtos/SignupDto.ts
@@ -0,0 +1,53 @@
+import { UserType } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  accessKey?: string;
+}
+
+export class SigninDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class GenerateAccessKeyDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsEnum(UserType)
+  @IsNotEmpty()
+  userType: UserType;
+}
